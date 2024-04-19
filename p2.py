d963c61035ca50b6da5b826e834a9af25e636f4d
xbiwas <biwas2059@gmail.com>
2024-04-19 18:15:36 +0545

    dsa

---
+++ p2.py
@@ -0,0 +1,24 @@
+# which data structure to use and when
+
+# Store apples price for 5 days
+
+apple_price = [230,123,432,456,321]
+print(apple_price[0],
+apple_price[3],
+apple_price[0:3],
+apple_price[1:3], #
+apple_price[::-1]) #it will reverse the array
+
+# store mango's price from april 19 to april 24
+
+mango_price = {
+    "April 19": 123,
+    "April 20": 456,
+    "April 21": 321,
+    "April 22": 789,
+    "April 23": 432,
+    "April 24": 451
+}
+
+print(mango_price["April 20"])
+
