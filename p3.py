0d606ce8941f853dd70ec454e9058f8b73c71b12
xbiwas <biwas2059@gmail.com>
2024-04-23 22:07:41 +0545

    Updated

---
+++ p3.py
@@ -0,0 +1,10 @@
+quiz = [
+    {
+       "Question":"What is the capital of Nepal?",
+        "Answer": "Nepal"
+    },
+    {
+       "Question":"What is the capital of India?",
+        "Answer": "Delhi"
+    },
+]
