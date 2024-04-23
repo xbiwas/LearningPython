b0fdec5af1fc2eb84d56288a387820ff7d3ec69f
xbiwas <biwas2059@gmail.com>
2024-04-23 22:18:28 +0545

    Updated

--- p3.py
+++ p3.py
@@ -1,7 +1,7 @@
 quiz = [
     {
        "Question":"What is the capital of Nepal?",
-        "Answer": "Nepal"
+        "Answer": "Kathmandu"
     },
     {
        "Question":"What is the capital of India?",
@@ -9,10 +9,22 @@ quiz = [
     },
 ]
 
-print("What is the capital of nepal")
 answer = input()
 
 if answer != quiz[0]["Answer"]:
     print("Wrong")
 else:
     print("Right")
+
+def check_answer(question, user_answer):
+    if user_answer != question["Answer"]:
+        return "WRONG"
+    else:
+        return "RIGHT"
+
+for question in quiz:
+    print(question["Question"])
+    userInput = input()
+    result = check_answer(question, userInput)
+    print(result)
+
