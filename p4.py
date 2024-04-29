b8f83270c7171992c67fff6eb880ca6fe61f1c4b
xbiwas <biwas2059@gmail.com>
2024-04-29 13:51:08 +0545

    Updated

--- p4.py
+++ p4.py
@@ -1,6 +1,25 @@
 import numpy as np
 
-a = np.array([1,2,3])
+a = np.array([1,2,3], dtype='int32')
 b = np.array([4,3,2])
+x = np.array([[1,2,3,4],[5,6,7,8]])
 
 print(a * b)
+
+# Get Dimension
+print(a.ndim)
+print(b.ndim)
+
+# Get shape
+print(a.shape)
+print(b.shape)
+
+# Get Data type
+print(a.dtype)
+print(b.dtype)
+
+# Get size
+print(a.itemsize)
+print(b.itemsize)
+
+print(x)
